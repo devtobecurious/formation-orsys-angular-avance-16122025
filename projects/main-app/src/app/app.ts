e96ca50832a0200ea117b29951286e007cc9bebf
Evan BOISSONNOT <evan@boissonnot.fr>
2025-12-17 15:39:58 +0100

    feat: mergemap and co

--- projects/main-app/src/app/app.ts
+++ projects/main-app/src/app/app.ts
@@ -1,7 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, ElementRef, signal, viewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DiscoverSignals } from './learnings/discover-signals/discover-signals';
 import { EditOneSelfie } from 'selfies';
+import { concatMap, fromEvent, interval, mergeMap, switchMap, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,35 @@ import { EditOneSelfie } from 'selfies';
 })
 export class App {
   protected readonly title = signal('main-app');
+  protected readonly counter = signal(0);
+  private startButton = viewChild<ElementRef<HTMLButtonElement>>('startButton');
+
+  constructor() {
+    // setInterval(() => {
+    //   this.counter.update((c) => c + 1);
+    // }, 1000);
+  }
+
+  ngOnInit() {
+    const button = this.startButton();
+
+    if (button) {
+      const obs$ = fromEvent(button.nativeElement, 'click');
+
+      // interval(1000).subscribe(
+      //   {
+      //     next: index => this.counter.set(index)
+      //   }
+      // )
+
+      obs$.pipe(
+        //mergeMap(() => interval(1000))
+        //concatMap(() => interval(1000)))
+        switchMap(() => interval(1000))
+      )
+      .subscribe({
+        next: count => this.counter.set(count)
+      });
+    }
+  }
 }
